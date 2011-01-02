cb1459d70e416b8d4961e30a4681baeb3c223d79
albertwcheng <albertwcheng@gmail.com>
2011-01-02 01:13:37 -0500

    commit

--- normalizeAgilentGPRFormattedArrayData.R
+++ normalizeAgilentGPRFormattedArrayData.R
@@ -12,7 +12,7 @@ RG$genes <- readGAL()
 RG$printer <- getLayout(RG$genes)
 
 # Preprocess data
-RG.b <-backgroundCorrect(RG,method="minimum")
+RG.b <-backgroundCorrect(RG,method="none",offset=0) #no background subtract. RG.b <-backgroundCorrect(RG,method="minimum")
 MA.p <-normalizeWithinArrays(RG.b, method="loess")
 MA.pq <- normalizeBetweenArrays(MA.p, method="quantile")
 RG.pq <- RG.MA(MA.pq)
@@ -25,7 +25,9 @@ setwd("normalized")
 # Write data
 plotDensities(RG.pq)
 data <- cbind(RG$genes, log2(RG.pq$R), log2(RG.pq$G))
-write.table(data, file = "complete_data.dat", sep="\t")
-plotDensities(RG.pq)
+write.table(data, file = "G.bgc0.norm.gn.txt", sep="\t")
+data.nolog <- cbind(RG$genes, RG.pq$R, RG.pq$G)
+write.table(data.nolog, file = "G.bgc0.norm.gn.nolog.txt", sep="\t")
+#plotDensities(RG.pq)
 
 setwd("..")
